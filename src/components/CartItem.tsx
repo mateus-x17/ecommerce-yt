339072f09bc55b499a0e5efbbad767a583d6dc70
mateus <mateusinaciorocha005@gmail.com>
2025-08-20 23:37:15 -0300

    create comp para cada item do carrinho

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,50 @@
+import { Trash2 } from "lucide-react";
+import Image from "next/image";
+import { CartItems } from "@/types";
+
+const CartItem = ({ item }: { item: CartItems }) => {
+  return (
+    // item unico carrinho
+    <div className="flex items-center justify-between" key={item.id}>
+      
+      {/* IMAGE E DETALHES */}
+      <div className="flex gap-8">
+        
+            {/* IMAGEM */}
+            <div className="relative w-32 h-32 bg-gray-50 overflow-hidden">
+            <Image
+                src={item.images[item.selectedColor]}
+                alt={item.name}
+                fill
+                className="object-contain"
+            />
+            </div>
+            
+            {/* DETALHES */}
+            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
+                <p className="text-sm font-medium">{item.name}</p>
+                <p className="text-xs text-gray-500">
+                Quantity: {""} {item.quantity}
+                </p>
+                <p className="text-xs text-gray-500">
+                Size: {""} {item.selectedSize}
+                </p>
+                <p className="text-xs text-gray-500">
+                Color: {""} {item.selectedColor}
+                </p>
+            </div>
+            <p className="font-medium">${item.price.toFixed(2)}</p>
+            </div>
+      </div>
+
+      {/* BOTÃO DELETE */}
+      <button className="w-8 h-8 rounded-full bg-red-100 text-red-500 flex items-center justify-center cursor-pointer hover:bg-red-200 transition-all duration-300">
+        <Trash2 className="w-3 h-3" />
+      </button>
+    
+    </div>
+  );
+};
+
+export default CartItem;
