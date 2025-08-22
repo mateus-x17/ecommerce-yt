903f22f230b9a7e501e92e3a697885076c88f9d1
mateus <mateusinaciorocha005@gmail.com>
2025-08-22 02:47:59 -0300

    adc popup do toastfy para mensagem ao adc item no carrinho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "../components/Footer";
 import NavBar from "../components/navBar";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,8 @@ export default function RootLayout({
           {children}
           <Footer />
           </div>
+        <ToastContainer position="bottom-right" /> 
+        {/* adc conteiner de menssagem do tastfy */}
       </body>
     </html>
   );
