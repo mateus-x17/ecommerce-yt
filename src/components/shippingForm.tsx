33b235450defc5016c8c3198a13a778e4b8f6176
mateus <mateusinaciorocha005@gmail.com>
2025-08-21 22:46:27 -0300

    criou formulario de entrega

--- src/components/shippingForm.tsx
+++ src/components/shippingForm.tsx
@@ -1,9 +1,124 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { shippingFormSchema, ShippingFormInputs } from "@/types";
+import { useRouter } from "next/navigation";
+import { ArrowRight } from "lucide-react";
 
+const ShippingForm = ({setShippingForm}: {setShippingForm:(data: ShippingFormInputs) => void}) => {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ShippingFormInputs>({
+        resolver: zodResolver(shippingFormSchema),
+    });
 
-const ShippingForm = () => {
-    return (
-        <div>ShippingForm</div>
-    )
-}
+    const handleShippingFomr: SubmitHandler<ShippingFormInputs> = (data) => {
+        setShippingForm(data);
+        console.log(data);
+        router.push("/cart?step=3", { scroll: false });
+    };
+
+  return (
+    <form className="flex flex-col gap-4"
+        onSubmit={handleSubmit(handleShippingFomr)}
+    >
+        {/* input nome */}
+        <div className="flex flex-col gap-1">
+        <label htmlFor="name" className="text-xs text-gray-500 font-medium">
+          Name
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="name"
+          placeholder="John Doe"
+          {...register("name")}
+        />
+        {errors.name && (
+          <p className="text-xs text-red-500">{errors.name.message}</p>
+        )}
+        </div>
+
+        {/* input email */}
+        <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="text-xs text-gray-500 font-medium">
+            Email
+            </label>
+            <input
+            className="border-b border-gray-200 py-2 outline-none text-sm"
+            type="email"
+            id="email"
+            placeholder="johndoe@gmail.com"
+            {...register("email")}
+            />
+            {errors.email && (
+            <p className="text-xs text-red-500">{errors.email.message}</p>
+            )}
+        </div>
+
+        {/* input phone */}
+        <div className="flex flex-col gap-1">
+            <label htmlFor="phone" className="text-xs text-gray-500 font-medium">
+            Phone
+            </label>
+            <input
+            className="border-b border-gray-200 py-2 outline-none text-sm"
+            type="text"
+            id="phone"
+            placeholder="123456789"
+            {...register("phone")}
+            />
+            {errors.phone && (
+            <p className="text-xs text-red-500">{errors.phone.message}</p>
+            )}
+        </div>
+
+        {/* input address */}
+        <div className="flex flex-col gap-1">
+            <label htmlFor="address" className="text-xs text-gray-500 font-medium">
+            Address
+            </label>
+            <input
+            className="border-b border-gray-200 py-2 outline-none text-sm"
+            type="text"
+            id="address"
+            placeholder="123 Main St, Anytown"
+            {...register("address")}
+            />
+            {errors.address && (
+            <p className="text-xs text-red-500">{errors.address.message}</p>
+            )}
+        </div>
+
+        {/* input city */}
+        <div className="flex flex-col gap-1">
+            <label htmlFor="city" className="text-xs text-gray-500 font-medium">
+            City
+            </label>
+            <input
+            className="border-b border-gray-200 py-2 outline-none text-sm"
+            type="text"
+            id="city"
+            placeholder="New York"
+            {...register("city")}
+            />
+            {errors.city && (
+            <p className="text-xs text-red-500">{errors.city.message}</p>
+            )}
+        </div>
+
+        <button
+            type="submit"
+            className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2"
+            >
+            Continue
+            <ArrowRight className="w-3 h-3" />
+        </button>
+    </form>
+)
+};
 
 export default ShippingForm;
+// onClick={()=> router.push("/cart?setep=2", { scroll: false })}
