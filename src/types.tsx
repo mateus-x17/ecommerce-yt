c4ebad3d363a5c1ae7b26df8505cb459b15ed2ed
mateus <mateusinaciorocha005@gmail.com>
2025-08-21 22:45:44 -0300

    adc type form pagamnetos e entrega

--- src/types.tsx
+++ src/types.tsx
@@ -1,4 +1,5 @@
-
+import { CiTrophy } from "react-icons/ci";
+import { email, z } from "zod";
 
 // tipagem dos produtos
 export type ProductType = {
@@ -24,3 +25,25 @@ export type CartItems = ProductType & {
 
 // tipagem do carrinho
 export type CartItemsType = CartItems[];
+
+//tipagem formulario de entrega
+export const shippingFormSchema = z.object({
+    name: z.string().min(1, 'O campo nome é obrigatório'),
+    email: z.string().min(1, 'O campo email é obrigatório'),
+    phone: z.string().min(7, 'O telefone deve ter pelo menos 7 dígitos').max(11, 'O telefone é obrigatório').regex(/^\d+$/, 'O telefone deve conter apenas números'),
+    address: z.string().min(1, 'O campo endereço é obrigatório'),
+    city: z.string().min(1, 'O campo cidade é obrigatório'),
+})
+//tipagem dos botões do fomrulario de entrega
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>
+
+
+//tipagem formulario de pagamneto
+export const paymentFormSchema = z.object({
+    cardHolder: z.string().min(1, 'Cartão de crédito obrigatório'),
+    cardNumber: z.string().min(16, 'numero de cartão de crédito obrigatorio').max(16, 'numero de cartão de crédito inválido'),
+    expirationDate: z.string().regex(/^(0[1-9]|1[0-2])\/\d{2}$/, 'Data de expiração inválida'),
+    cvv: z.string().min(3).max(3,'CVV inválido'),
+})
+//tipagem dos botões do fomrulario de pagamento
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>
