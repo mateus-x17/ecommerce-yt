0a2c5bd2d6de35d3f900bdf575f3f63f146ba726
mateus <mateusinaciorocha005@gmail.com>
2025-08-22 02:45:50 -0300

    adc contexto para contar itens no carrinho

--- src/components/shoppingCartIcon.tsx
+++ src/components/shoppingCartIcon.tsx
@@ -1,12 +1,18 @@
+"use client" //pois o contexto do carrinho precisa de reatividade
+
 import { ShoppingCart } from "lucide-react"
 import Link from "next/link"
+import useCartStore from "@/context/cartContext";
 
 const ShoppingCartIcon = () =>{
+    const { cart, hasHydrated } = useCartStore(); //acessar o contexto do carrinho
+    if(!hasHydrated) return null;
 
     return(
         <Link href="/cart" className="relative">
             <ShoppingCart className="w-4 h-4 text-gray-600"/>
-            <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-5 h-4 p-1 flex justify-center items-center text-xs">0</span>
+            <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-5 h-4 p-1 flex justify-center items-center text-xs"><b>{cart.reduce((acc, item) => acc + item.quantity, 0)}</b></span>
+            {/* exibe a quantidade de itens no carrinho */}
         </Link>
     )
 };
